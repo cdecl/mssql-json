3cd4319d168ced68c6b6de20f0d6179b44655fbf
Byung Kyu KIM <cdeclare@gmail.com>
2018-12-15 11:00:34 +0900

    Refactoring

--- mssql-json.py
+++ mssql-json.py
@@ -3,28 +3,37 @@
 import pymssql
 import json
 
+class config:
+	output_file = r"output.json"
+	sql = '''
+			select *
+			from tblname
+	'''
+
 def db_connect():
 	return pymssql.connect(server='SERVER-NAME', user='', password='', database='', charset='utf8')  
 
 def main():
-	output_file = r"d:\a.json"
-	sql = '''
-			select *
-			from tblname
-		'''
-		
 	with db_connect() as conn:
-		with open(output_file, 'w') as fout:
+		with open(config.output_file, 'w') as fout:
 			cur = conn.cursor(as_dict=True)
-			cur.execute(sql)  
+			cur.execute(config.sql)  
 			row = cur.fetchone()
 
-			while row:  
-				fout.write(json.dumps(row, ensure_ascii=False))
-				fout.write('\n')
-				row = cur.fetchone()  
-
+			trans_data(row, fout, cur)
 	print('Done')
+
+def trans_data(row, fout, cur):
+	idx = 0
+	while row:  
+		fout.write(json.dumps(row, ensure_ascii=False), '\n')
+		idx += 1
+
+		if (idx % 100000) == 0:
+			print('rows : {0}'.format(idx))
+		row = cur.fetchone() 
+
+	print('rows : {0}'.format(idx))
 
 if __name__ == '__main__':
 	main()
