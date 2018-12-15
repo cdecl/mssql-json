15d8bb839ea0d0c0ca776b1eb8d671486be77ed3
N15479 <cdeclare@interpark.com>
2018-12-15 11:38:09 +0900

    Refactoring

--- mssql-json.py
+++ mssql-json.py
@@ -6,12 +6,13 @@ import json
 class config:
 	output_file = r"output.json"
 	sql = '''
-			select *
-			from tblname
+		select *
+		from tbl
 	'''
 
 def db_connect():
-	return pymssql.connect(server='SERVER-NAME', user='', password='', database='', charset='utf8')  
+	return pymssql.connect(server='STAGE1', user='user', password='pwd', database='dbname', charset='utf8')  
+
 
 def main():
 	with db_connect() as conn:
@@ -24,16 +25,30 @@ def main():
 	print('Done')
 
 def trans_data(row, fout, cur):
-	idx = 0
-	while row:  
-		fout.write(json.dumps(row, ensure_ascii=False), '\n')
-		idx += 1
+	buffer_size = 1000
+	display_size = 10000
 
-		if (idx % 100000) == 0:
-			print('rows : {0}'.format(idx))
+	idx = 0
+	doc = []
+	while row:
+		doc.append(json.dumps(row, ensure_ascii=False) + '\n')
 		row = cur.fetchone() 
+		idx += 1
+		
+		if len(doc) == buffer_size:
+			fout.writelines(doc)
+			doc.clear()
+
+		if (idx % display_size) == 0:
+			print('Rows : {0}'.format(idx))
+	
+	if len(doc) > 0:
+		fout.writelines(doc)
+		doc.clear()
 
 	print('rows : {0}'.format(idx))
+
+
 
 if __name__ == '__main__':
 	main()
